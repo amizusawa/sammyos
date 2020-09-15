1c28d11f6d7c2cf1ff0659a70a31347143fd133d
akihiro <aki.mizu101@gmail.com>
2020-09-15 14:16:11 -0700

    irq handlers

--- cpu/isr.c
+++ cpu/isr.c
@@ -3,10 +3,29 @@
 #include "../drivers/screen.h"
 #include "../kernel/utils.h"
 
+isr_t interrupt_handlers[256];
+
+void register_interrupt_handler(u8 n, isr_t handler) {
+    interrupt_handlers[n] = handler;
+}
+
 void isr_handler(registers_t regs) {
     kprint("received interrupt: ");
     char buff[10];
     int_to_ascii(regs.int_no, buff);
     kprint(buff);
     kprint("\n");
+}
+
+void irq_handler(registers_t regs) {
+    if (regs.int_no >= 40) {
+        port_byte_out(0xA0, 0x20);
+    }
+
+    port_byte_out(0x20, 0x20);
+
+    if (interrupt_handlers[regs.int_no] != 0) {
+        isr_t handler = interrupt_handlers[regs.int_no];
+        handler(regs);
+    }
 }
