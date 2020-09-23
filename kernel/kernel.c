3b93954b1dc9f20f3a981bdbd7a20f36526a0a58
akihiro <aki.mizu101@gmail.com>
2020-09-23 14:37:57 -0700

    added multiboot header from the multiboot specifications

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,14 @@
 #include "kernel.h"
+#include "multiboot.h"
 #include "../drivers/screen.h"
 #include "../cpu/descriptor_tables.h"
 #include "../drivers/timer.h"
 #include "../drivers/keyboard.h"
 
-#define MBOOT_LOADER_MAGIC 0x2badb002
 
 void kernel_main(uint32_t mboot_magic, void* mboot_header) {
 
-    if (mboot_magic != MBOOT_LOADER_MAGIC) {
+    if (mboot_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
         kprint("ERROR: Kernel loaded by non-compliant bootloader\n");
         return;
     }
