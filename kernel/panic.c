d53f612d8c02b0b92bc9016eeeb38ebc1baa44b8
Akihiro Mizusawa <aki.mizu101@gmail.com>
2021-01-17 17:36:02 -0800

    basic kernel panics ; assert and not_reached macros

---
+++ kernel/panic.c
@@ -0,0 +1,15 @@
+#include "panic.h"
+#include <drivers/screen.h>
+
+// Print kernel panic and halt cpu
+void kpanic(char* reason) {
+    screen_clear();
+    kprint("KERNEL PANIC: ");
+    kprint(reason);
+
+    asm __volatile__("hlt");
+
+    //TODO: eventually print backtrace info
+    
+}
+
--- libc/function.h
+++ libc/function.h
@@ -1,6 +1,15 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
+#include <kernel/panic.h>
+
 #define UNUSED(x) (void) (x);
+#define NOT_REACHED() kpanic("Reached an unreachable statement");
+
+//TODO Make assertion failure message actually useful
+#define ASSERT(condition) \
+    if (!(condition)) {\
+        kpanic("Assertion failed"); \
+    }
 
 #endif
