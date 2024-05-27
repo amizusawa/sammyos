44ecfbb1f9086b0c11d6ffd2cd3e1808f77f0b39
Akihiro Mizusawa <aki.mizu101@gmail.com>
2024-05-26 21:58:08 -0500

    Build: Add envsetup.sh script
    Add new envsetup.sh script. This script must be run in the sammyos
    root directory. This script will set needed environment variables,
    and define the "smk" (sammyos make) macro, which allows running
    makefile commands from any directory.

---
+++ envsetup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+BASENAME=$(echo ${CURRENT_DIR} | grep -oE '[^/]+$')
+if [ ${BASENAME} != "sammyos" ]; then
+    echo "Please run \". envsetup.sh\" from the sammyos root directory."
+    exit 1
+else
+    export SAMMYOSROOT=${CURRENT_DIR}
+    echo "SAMMYOSROOT set to ${SAMMYOSROOT}"
+fi
+
+if [ -z "$1" ]; then
+    export SAMMYOSARCH=i386
+    echo "Target architecture not specified. Defaulting to i386"
+elif [ $1 == "i386" ]; then
+    export SAMMYOSARCH=i386
+    echo "Setting target architecture to i386."
+else
+    echo "Unknown target architecture. Aborting."
+    exit 1
+fi
+
+echo "You are able to use \"smk\" to build sammyos from any directory."
+smk() {
+    cd ${SAMMYOSROOT}
+    make $1
+    cd -
+}
